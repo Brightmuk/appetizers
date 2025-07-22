e32a52b4727fa554fbfd735bcc40297c7d395254
Brimuk <be.rightmuk@gmail.com>
2025-07-22 22:11:08 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Bright Mukonesi on 22/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
